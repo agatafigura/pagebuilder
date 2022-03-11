dd0db4516612b7f35c0612cb0b67299779bde628
Agata Figura <agata.figura1@gmail.com>
2022-03-11 10:12:08 +0100

    .

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -50,12 +50,10 @@
                       </label>
                       <div class="flex flex-col w-full">
                         <div
-                          v-for="icon in icons"
-                          :key="icon"
                           class="flex gap-x-1 w-full mt-3"
                         >
                           <svg class="fill-current text-gray-600 w-7 h-7">
-                            <path class="w-5 h-5" :d="icon" />
+                            <path class="w-5 h-5" d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                           </svg>
 
                           <input
@@ -63,19 +61,165 @@
                             @keydown="handleKeyDown($event)"
                             class="border border-gray-600 rounded outline-none p-0.5"
                             placeholder="http://example.com"
+                            v-model="test"
                           />
 
                           <div class="flex gap-x-2">
                             <CIcon 
                               class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
                               :icon="cilCheckAlt"
-                              @click="addIcon($event, icon)"
+                              @click="addIcon($event)"
                             />
 
                             <CIcon 
-                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer hidden"
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
                               :icon="cilTrash"
-                              @click="removeIcon($event, icon)"
+                              @click="removeIcon($event)"
+                            />
+                          </div>
+                        </div>
+                        <div
+                          class="flex gap-x-1 w-full mt-3"
+                        >
+                          <svg class="fill-current text-gray-600 w-7 h-7">
+                            <path class="w-5 h-5" d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z" />
+                          </svg>
+
+                          <input
+                            @click="handleClick($event)"
+                            @keydown="handleKeyDown($event)"
+                            class="border border-gray-600 rounded outline-none p-0.5"
+                            placeholder="http://example.com"
+                             
+                          />
+
+                          <div class="flex gap-x-2">
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
+                              :icon="cilCheckAlt"
+                              @click="addIcon($event)"
+                            />
+
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
+                              :icon="cilTrash"
+                              @click="removeIcon($event)"
+                            />
+                          </div>
+                        </div>
+                        <div
+                          class="flex gap-x-1 w-full mt-3"
+                        >
+                          <svg class="fill-current text-gray-600 w-7 h-7">
+                            <path class="w-5 h-5" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                          </svg>
+
+                          <input
+                            @click="handleClick($event)"
+                            @keydown="handleKeyDown($event)"
+                            class="border border-gray-600 rounded outline-none p-0.5"
+                            placeholder="http://example.com"
+                            
+                          />
+
+                          <div class="flex gap-x-2">
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
+                              :icon="cilCheckAlt"
+                              @click="addIcon($event)"
+                            />
+
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
+                              :icon="cilTrash"
+                              @click="removeIcon($event)"
+                            />
+                          </div>
+                        </div>
+                        <div
+                          class="flex gap-x-1 w-full mt-3"
+                        >
+                          <svg class="fill-current text-gray-600 w-7 h-7">
+                            <path class="w-5 h-5" d= "M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                          </svg>
+
+                          <input
+                            @click="handleClick($event)"
+                            @keydown="handleKeyDown($event)"
+                            class="border border-gray-600 rounded outline-none p-0.5"
+                            placeholder="http://example.com"
+                             
+                          />
+
+                          <div class="flex gap-x-2">
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
+                              :icon="cilCheckAlt"
+                              @click="addIcon($event)"
+                            />
+
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
+                              :icon="cilTrash"
+                              @click="removeIcon($event)"
+                            />
+                          </div>
+                        </div>
+                        <div
+                          class="flex gap-x-1 w-full mt-3"
+                        >
+                          <svg class="fill-current text-gray-600 w-7 h-7">
+                            <path class="w-5 h-5" d= "M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+                          </svg>
+
+                          <input
+                            @click="handleClick($event)"
+                            @keydown="handleKeyDown($event)"
+                            class="border border-gray-600 rounded outline-none p-0.5"
+                            placeholder="http://example.com"
+                             
+                          />
+
+                          <div class="flex gap-x-2">
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
+                              :icon="cilCheckAlt"
+                              @click="addIcon($event)"
+                            />
+
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
+                              :icon="cilTrash"
+                              @click="removeIcon($event)"
+                            />
+                          </div>
+                        </div>
+                        <div
+                          class="flex gap-x-1 w-full mt-3"
+                        >
+                          <svg class="fill-current text-gray-600 w-7 h-7">
+                            <path class="w-5 h-5" d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z" />
+                          </svg>
+
+                          <input
+                            @click="handleClick($event)"
+                            @keydown="handleKeyDown($event)"
+                            class="border border-gray-600 rounded outline-none p-0.5"
+                            placeholder="http://example.com"
+                             
+                          />
+
+                          <div class="flex gap-x-2">
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-green-600 cursor-pointer"
+                              :icon="cilCheckAlt"
+                              @click="addIcon($event)"
+                            />
+
+                            <CIcon 
+                              class="w-4 opacity-100 hover:opacity-70 hover:text-red-600 cursor-pointer  "
+                              :icon="cilTrash"
+                              @click="removeIcon($event)"
                             />
                           </div>
                         </div>
@@ -330,6 +474,7 @@ export default {
   },
 
   setup(props) {
+    const test = ref(null);
     //font size
     const fontSize = ref("");
     const sizes = ref([
@@ -595,26 +740,24 @@ export default {
     //adding SoMe icons
 
     const div = ref(null);
-    const TwitterInput = ref(true);
-    const TwitterAdd = ref(true);
-    const TwitterDelete = ref(false);
 
-
-    const addIcon = function (e, icon) {
+    const addIcon = function (e) {
       const icons = document.querySelector("#icon-list");
       const inputField = e.currentTarget.parentElement.previousElementSibling;
-      const iconAdd = e.currentTarget;
-      const iconRemove = e.currentTarget.nextElementSibling;
       const inputValue = inputField.value;
+      const svg = e.currentTarget.parentElement.previousElementSibling.previousElementSibling;
+      const path = svg.querySelector("path");
+      const d = path.getAttribute("d");
       
       //check if icon is added and validate link
       if (
-        icons.getElementsByClassName(`${icon}`).length < 1 &&
+        icons.getElementsByClassName(`${d}`).length < 1 &&
         inputValue.length > 7
       ) {
         //create div and append in list
         div.value = document.createElement("div");
         div.value.classList = "w-10 h-10 flex justify-center items-center";
+        div.value.className += d;
         icons.appendChild(div.value);
 
         //wrap div in an anchor tag
@@ -651,17 +794,11 @@ export default {
           "http://www.w3.org/2000/svg",
           "path"
         );
-        newPath.setAttributeNS(null, "d", icon);
+        newPath.setAttributeNS(null, "d", d);
         newIcon.appendChild(newPath);
-        let d = newPath.getAttribute("d");
         div.value.className += " " + d;
-
-        //remove input field and icon
-        iconAdd.style.display = "none";
-        inputField.style.display = "none";
-        iconRemove.style.display = "block";
-
-        //set icon as added
+        console.log(test.value);
+        
 
         //show error
       } else if (inputValue.length <= 7) {
@@ -671,25 +808,22 @@ export default {
     };
 
     //remove icons
-    const removeIcon = function (e, icon) {
-      const icons = document.querySelector("#icon-list");
+    const removeIcon = function (e) {
       const inputField = e.currentTarget.parentElement.previousElementSibling;
-      const iconAdd = e.currentTarget.previousElementSibling;
-      const iconRemove = e.currentTarget;
+      const icons = document.querySelector("#icon-list");
+      const svg = e.currentTarget.parentElement.previousElementSibling.previousElementSibling;
+      const path = svg.querySelector("path");
+      const d = path.getAttribute("d");
 
       //for each icon
       icons.querySelectorAll("div").forEach((div) => {
-        let svg = div.querySelector("svg");
-        let path = svg.querySelector("path");
-        let d = path.getAttribute("d");
-        if (d === icon) {
+        let thisSvg = div.querySelector("svg");
+        let thisPath = thisSvg.querySelector("path");
+        let thisD = thisPath.getAttribute("d");
+        if (thisD === d) {
           //remove div
-          path.parentElement.parentElement.parentElement.remove();
-          //add input field and icon
-          iconRemove.style.display = "none";
-          iconAdd.style.display = "block";
-          inputField.style.display = "block";
-          inputField.value = "";
+          thisPath.parentElement.parentElement.parentElement.remove();
+          console.log(test.value);
         }
       });
     };
@@ -761,13 +895,4 @@ export default {
 
 
 <style>
-
-  .hide {
-    display: none;
-  }
-
-  .show {
-    display: block;
-  }
-
 </style>
